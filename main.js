da1c44f4d1d9ca3bac5b33d0e6b72f61e6ab74da
Nick Eagan <nick.eagan@timeinc.com>
2016-04-08 11:16:47 -0700

    basic electron outline

---
+++ main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const electron = require('electron');
+// Application life cycle module
+const app = electron.app;
+// Browser window creation module
+const BrowserWindow = electron.BrowserWindow;
+
+// Global reference to main window object
+let mainWindow;
+
+function createWindow() {
+  // Create new browser window
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // Load index.html from app
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Open dev tools
+  mainWindow.webContents.openDevTools();
+
+  // Close window event
+  mainWindow.on('closed', function() {
+    // Deference all windows
+    mainWindow = null;
+  });
+}
+
+// Create window after electron initialization
+app.on('ready', createWindow);
+
+// Quit app when windows are closed
+app.on('window-all-closed', function() {
+  // OS X apps stay active until user explicitly quits
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+
+app.on('activate', function() {
+  // OS X app recreates window when dock icon is clicked
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
